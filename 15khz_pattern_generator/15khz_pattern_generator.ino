0e59ee0955651ead112b98e810ff08fb6ff21c37
Olivier Le Cam <olivier.lecam@ac-versailles.fr>
2020-11-08 11:35:07 +0100

    add some explanations

--- 15khz_pattern_generator/15khz_pattern_generator.ino
+++ 15khz_pattern_generator/15khz_pattern_generator.ino
@@ -2,4 +2,17 @@
 // Designed for Arduino Nano/Uno
 // (c) Olivier Le Cam <olecam@glou.fr>
 //
-// Project is pure assembly, this why this ino file is empty.
+// Project is pure assembly, this is why this ino file is empty.
+//
+// Outputs:
+//    PD2: Red
+//    PD3: Green
+//    PD4: Blue
+//    PD5: V.SYNC (vertical synchronization signal)
+//    PD6: H.SYNC (horizontal synchronization signal)
+//    PD7: C.SYNC (combines both V.SYNC and H.SYNC)
+//
+// Arduino outputs can be be wired directly to the Monitor or via a buffer
+// (LS125, LS367, ...) in order to protect your Arduino.
+// C.SYNC combines both vertical and horizontal sync signals on a sigle wire.
+// Most of the time, like with Hantarex Monitors, you can just wire C.SYNC. 
